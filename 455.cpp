66d1cf5c0498d49d6c0633195e879223ddb54e12
gokul0410 <gokuldhana04@gmail.com>
2026-04-25 17:34:07 +0530

    Atcoder Contest problem

---
+++ 455.cpp
@@ -0,0 +1,36 @@
+// Problem: A - 455
+// Contest: AtCoder - Ｓｋｙ Inc, Programming Contest 2026 (AtCoder Beginner Contest 455)
+// URL: https://atcoder.jp/contests/abc455/tasks/abc455_a
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+using ll = long long;
+
+void solve() {
+    int A,B,C;
+    cin>>A>>B>>C;
+    if(A!=B && B==C) cout<<"Yes";
+    else cout<<"No";
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t; // Toggle this based on problem format
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
